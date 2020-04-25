48c2bc1be7fd896e71c9ebac1ed3bcea2cbb8255
minkj1992 <minkj1992@gmail.com>
2020-04-25 16:32:53 +0900

    add sign-up controller

--- src/main/java/com/minkj1992/bluejeju/account/AccountController.java
+++ src/main/java/com/minkj1992/bluejeju/account/AccountController.java
@@ -1,20 +1,17 @@
 package com.minkj1992.bluejeju.account;
 
-import com.minkj1992.bluejeju.domain.Account;
-import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path="/account")
-@RequiredArgsConstructor
 public class AccountController {
 
     @GetMapping(value="/sign-up")
-    public String createForm(Model model) {
-        model.addAttribute("AccountForm", new AccountForm());
+    public String signUpForm(Model model) {
+//        model.addAttribute("AccountForm", new AccountForm());
         return "account/sign-up";
     }
 }
